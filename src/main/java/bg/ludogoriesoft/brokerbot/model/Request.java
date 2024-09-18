b69328f80d0d29c449cf266645be2b0458744eb4
Ivo <ivailo.yordanov.ph@gmail.com>
2024-09-19 00:11:21 +0300

    add missing spaces and fix * imports

--- src/main/java/bg/ludogoriesoft/brokerbot/model/Request.java
+++ src/main/java/bg/ludogoriesoft/brokerbot/model/Request.java
@@ -1,20 +1,21 @@
 package bg.ludogoriesoft.brokerbot.model;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @NoArgsConstructor
 @Getter
 @Setter
 @ToString
 public class Request {
-
     private String phoneNumber;
     private String assistantName;
-    private String prompt = "Ти си асистент на брокер." +
-            "Твоята роля е да звъниш на клиенти и да ги питаш дали" +
+    private String prompt = "Ти си асистент на брокер. " +
+            "Твоята роля е да звъниш на клиенти и да ги питаш дали " +
             "днес им е удобно да направят оглед на имот. В случай, че не им е удобно ги " +
             "попитай дали ще имат тази възможност в следващите няколко дни. " +
             "Ако ти откажат им кажи, че ще се свържеш пак с тях следващите дни.";
     private String propertyInfo;
-
 }
