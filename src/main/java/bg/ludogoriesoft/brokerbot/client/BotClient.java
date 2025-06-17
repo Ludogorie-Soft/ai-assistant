1c5e34d243b4b670d9f28336c9e2595fbca49185
Denis <denis.irhan@gmail.com>
2025-06-17 12:01:58 +0300

    Remove global annotation - @Component

--- src/main/java/bg/ludogoriesoft/brokerbot/client/BotClient.java
+++ src/main/java/bg/ludogoriesoft/brokerbot/client/BotClient.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@Component
 @FeignClient(name = "bot-client", url = "https://api.bland.ai",
         configuration = CustomRequestInterceptor.class)
 public interface BotClient {
