c39070a3e0b2f619f2ae5cddbd2c592372b7414d
Denis <denis.irhan@gmail.com>
2025-06-17 16:33:43 +0300

    Change phone number id

--- src/main/java/bg/ludogoriesoft/brokerbot/modelElevenLabs/CallBodyElevenLabs.java
+++ src/main/java/bg/ludogoriesoft/brokerbot/modelElevenLabs/CallBodyElevenLabs.java
@@ -12,7 +12,7 @@ import lombok.ToString;
 public class CallBodyElevenLabs {
 
     private String agent_id = "agent_01jxy9g3rbfsjsev7faxxd6vdv";
-    private String agent_phone_number_id = "phnum_01jxybm3nxea88kk73p2ty3aj2";
+    private String agent_phone_number_id = "phnum_01jxz13wtzfpjs8fanpkf9ynsc";
     private String to_number;
     public ConversationInitiationClientData conversation_initiation_client_data;
 }
