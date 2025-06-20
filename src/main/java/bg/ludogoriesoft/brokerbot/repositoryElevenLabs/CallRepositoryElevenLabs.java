72ac38c9a8008d18dabe0fa23d63841bed4598f1
Denis <denis.irhan@gmail.com>
2025-06-20 11:38:33 +0300

    Add CallRepositoryElevenLabs

---
+++ src/main/java/bg/ludogoriesoft/brokerbot/repositoryElevenLabs/CallRepositoryElevenLabs.java
@@ -0,0 +1,20 @@
+package bg.ludogoriesoft.brokerbot.repositoryElevenLabs;
+
+import bg.ludogoriesoft.brokerbot.modelElevenLabs.CallElevenLabs;
+import bg.ludogoriesoft.brokerbot.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface CallRepositoryElevenLabs extends JpaRepository<CallElevenLabs, Long> {
+    List<CallElevenLabs> findByUser(User user);
+    List<CallElevenLabs> findByUserAndIsVisitConfirmedTrue(User user);
+
+    @Query("SELECT c FROM CallElevenLabs c WHERE c.user = :user AND c.isVisitConfirmed = false")
+    List<CallElevenLabs> findByUserAndNotConfirmed(@Param("user") User user);
+
+    @Query("SELECT c FROM CallElevenLabs c WHERE c.user = :user AND c.isVisitConfirmed IS NULL")
+    List<CallElevenLabs> findUnansweredCallsByUser(@Param("user") User user);
+}
