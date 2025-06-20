5d0a34d05534242f90fd376f4ec5aa160a9f1ce8
Denis <denis.irhan@gmail.com>
2025-06-20 11:39:10 +0300

    Add CallElevenLabs Entity

---
+++ src/main/java/bg/ludogoriesoft/brokerbot/modelElevenLabs/CallElevenLabs.java
@@ -0,0 +1,42 @@
+package bg.ludogoriesoft.brokerbot.modelElevenLabs;
+
+import bg.ludogoriesoft.brokerbot.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Builder
+@AllArgsConstructor
+@Table(name = "calls_eleven_labs")
+public class CallElevenLabs {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @JoinColumn(name = "user_id")
+    @ManyToOne
+    private User user;
+
+    private String conversation_id;
+
+    private Boolean isVisitConfirmed;
+
+    @Column(columnDefinition = "TEXT")
+    private String summary;
+
+    @Column(name = "to_number")
+    private String to;
+
+    private double call_duration_secs;
+    private LocalDate callDate;
+    private LocalTime callTime;
+}
