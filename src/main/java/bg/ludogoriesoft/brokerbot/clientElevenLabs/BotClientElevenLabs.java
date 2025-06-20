94cb5a296e217900f664dc3a2830ff36565960e8
Denis <denis.irhan@gmail.com>
2025-06-20 11:46:30 +0300

    Add Get conversation info endpoint by id

--- src/main/java/bg/ludogoriesoft/brokerbot/clientElevenLabs/BotClientElevenLabs.java
+++ src/main/java/bg/ludogoriesoft/brokerbot/clientElevenLabs/BotClientElevenLabs.java
@@ -1,9 +1,12 @@
 package bg.ludogoriesoft.brokerbot.clientElevenLabs;
 
 import bg.ludogoriesoft.brokerbot.modelElevenLabs.CallBodyElevenLabs;
+import bg.ludogoriesoft.brokerbot.modelElevenLabs.CallDataResponseElevenLabs;
 import bg.ludogoriesoft.brokerbot.modelElevenLabs.CallResponseElevenLabs;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -14,4 +17,7 @@ public interface BotClientElevenLabs {
 
     @PostMapping("/v1/convai/twilio/outbound-call")
     ResponseEntity<CallResponseElevenLabs> makeCall(@RequestBody CallBodyElevenLabs callBodyElevenLabs);
+
+    @GetMapping("/v1/convai/conversations/{conv_id}")
+    ResponseEntity<CallDataResponseElevenLabs> getCallInfo(@PathVariable("conv_id") String id);
 }
