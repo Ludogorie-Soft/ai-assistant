ad71d855b52d2109363a537e40749e8a13cb5424
Denis <denis.irhan@gmail.com>
2025-06-20 11:42:37 +0300

    Add search from repository methods

--- src/main/java/bg/ludogoriesoft/brokerbot/serviceElevenLabs/BotServiceElevenLabs.java
+++ src/main/java/bg/ludogoriesoft/brokerbot/serviceElevenLabs/BotServiceElevenLabs.java
@@ -2,22 +2,35 @@ package bg.ludogoriesoft.brokerbot.serviceElevenLabs;
 
 import bg.ludogoriesoft.brokerbot.clientElevenLabs.BotClientElevenLabs;
 import bg.ludogoriesoft.brokerbot.exception.InvalidPhoneNumberException;
+import bg.ludogoriesoft.brokerbot.exception.UserNotAuthenticatedException;
+import bg.ludogoriesoft.brokerbot.model.Call;
 import bg.ludogoriesoft.brokerbot.modelElevenLabs.*;
+import bg.ludogoriesoft.brokerbot.repository.UserRepository;
+import bg.ludogoriesoft.brokerbot.repositoryElevenLabs.CallRepositoryElevenLabs;
+import bg.ludogoriesoft.brokerbot.service.ASyncService;
+import bg.ludogoriesoft.brokerbot.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 public class BotServiceElevenLabs {
     private final BotClientElevenLabs botClientElevenLabs;
+    private final UserRepository userRepository;
+    private final CallRepositoryElevenLabs callRepositoryElevenLabs;
+    private final ASyncServiceElevenLabs aSyncServiceElevenLabs;
 
-    public Map<String, Object> processCall(RequestElevenLabs requestDto) {
-
+    public Map<String, Object> processCall(RequestElevenLabs requestDto, String email) {
+        User user = getUserOrThrow(email);
         ResponseEntity<CallResponseElevenLabs> response = makeCall(requestDto);
+
+        aSyncServiceElevenLabs.waitForCallAndProcess(Objects.requireNonNull(response.getBody()).getConversation_id(), user);
 
         Map<String, Object> responseBody = new HashMap<>();
         responseBody.put("call_id", response.getBody().getConversation_id());
@@ -44,4 +57,26 @@ public class BotServiceElevenLabs {
         }
         return "+359" + phoneNumber;
     }
+
+    private User getUserOrThrow(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UserNotAuthenticatedException("User not authenticated!"));
+    }
+
+    public List<CallElevenLabs> getMyCalls(String email) {
+        return callRepositoryElevenLabs.findByUser(getUserOrThrow(email));
+    }
+
+    public List<CallElevenLabs> getMySuccessfulCalls(String email) {
+        return callRepositoryElevenLabs.findByUserAndIsVisitConfirmedTrue(getUserOrThrow(email));
+    }
+
+    public List<CallElevenLabs> getMyUnansweredCalls(String email) {
+        return callRepositoryElevenLabs.findUnansweredCallsByUser(getUserOrThrow(email));
+    }
+
+    public List<CallElevenLabs> getMyUnsuccessfulCalls(String email) {
+        return callRepositoryElevenLabs.findByUserAndNotConfirmed(getUserOrThrow(email));
+    }
+
 }
