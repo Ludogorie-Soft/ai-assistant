afe51155b94e733ebf2b678aba0fb3ef5952e246
Denis <denis.irhan@gmail.com>
2025-07-02 22:27:01 +0300

    Add new query for unansweredCallsByNumber and lastAnsweredCallByNumber

--- src/main/java/bg/ludogoriesoft/brokerbot/repositoryElevenLabs/CallRepositoryElevenLabs.java
+++ src/main/java/bg/ludogoriesoft/brokerbot/repositoryElevenLabs/CallRepositoryElevenLabs.java
@@ -6,7 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
+import java.util.Map;
 
 public interface CallRepositoryElevenLabs extends JpaRepository<CallElevenLabs, Long> {
     List<CallElevenLabs> findByUser(User user);
@@ -23,4 +27,10 @@ public interface CallRepositoryElevenLabs extends JpaRepository<CallElevenLabs, 
 
     @Query("SELECT c FROM CallElevenLabs c WHERE c.convId = :conv_id")
     CallElevenLabs findConversationsByConvId(@Param("conv_id") String conv_id);
+
+    @Query("SELECT c FROM CallElevenLabs c WHERE c.to = :number AND (c.callDate + c.callTime) > :date AND c.isVisitConfirmed IS NULL")
+    List<CallElevenLabs> findUnansweredCallsByNumber(@Param("number") String number, @Param("date") LocalDateTime date);
+
+    @Query("SELECT c FROM CallElevenLabs c WHERE c.to = :number AND c.isVisitConfirmed IS NOT NULL ORDER BY c.callDate ASC, c.callTime ASC")
+    List<CallElevenLabs> findLastAnsweredCallByNumber(@Param("number") String number);
 }
