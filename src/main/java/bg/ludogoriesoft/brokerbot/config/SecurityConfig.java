9138f8fed5b106cbc85aef3c1a2acced476f81a6
Denis <denis.irhan@gmail.com>
2025-07-02 22:25:52 +0300

    Add new page for authenticated()

--- src/main/java/bg/ludogoriesoft/brokerbot/config/SecurityConfig.java
+++ src/main/java/bg/ludogoriesoft/brokerbot/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/","/register", "/login", "/history", "/bland").permitAll()
-                        .requestMatchers("/**", "/eleven-labs/call").authenticated()
+                        .requestMatchers("/**", "/eleven-labs/call", "/eleven-labs/num-history").authenticated()
                 )
                 .formLogin(form -> form
                         .loginPage("/login")
